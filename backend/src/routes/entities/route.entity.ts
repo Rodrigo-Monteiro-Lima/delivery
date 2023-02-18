fac8645b35ad0c32306ef1db37d45cff502a71be
Rodrigo <rodrigoml2395@gmail.com>
2023-02-18 16:59:32 -0300

    feat: route entity

--- backend/src/routes/entities/route.entity.ts
+++ backend/src/routes/entities/route.entity.ts
@@ -1,1 +1,31 @@
-export class Route {}
+import { Prop, Schema, raw, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type RouteDocument = Route & Document;
+
+@Schema()
+export class Route {
+  @Prop()
+  _id: string;
+
+  @Prop()
+  title: string;
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  startPosition: { lat: number; lng: number };
+
+  @Prop(
+    raw({
+      lat: { type: Number },
+      lng: { type: Number },
+    }),
+  )
+  endPosition: { lat: number; lng: number };
+}
+
+export const RouteSchema = SchemaFactory.createForClass(Route);

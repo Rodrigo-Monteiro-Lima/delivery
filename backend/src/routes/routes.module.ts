ce4b776ac1c7aa711b241211d1b9b3a196c57f43
Rodrigo <rodrigoml2395@gmail.com>
2023-02-18 17:39:39 -0300

    feat: kafka connection

--- backend/src/routes/routes.module.ts
+++ backend/src/routes/routes.module.ts
@@ -2,11 +2,33 @@ import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { RouteSchema, Route } from './entities/route.entity';
+import { Route, RouteSchema } from './entities/route.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+    ClientsModule.registerAsync([
+      {
+        name: 'KAFKA_SERVICE',
+        useFactory: (): any => ({
+          transport: Transport.KAFKA,
+          options: {
+            client: {
+              clientId: process.env.KAFKA_CLIENT_ID,
+              brokers: [process.env.KAFKA_BROKER],
+            },
+            consumer: {
+              groupId:
+                !process.env.KAFKA_CONSUMER_GROUP_ID ||
+                process.env.KAFKA_CONSUMER_GROUP_ID === ''
+                  ? 'my-consumer-' + Math.random()
+                  : process.env.KAFKA_CONSUMER_GROUP_ID,
+            },
+          },
+        }),
+      },
+    ]),
   ],
   controllers: [RoutesController],
   providers: [RoutesService],

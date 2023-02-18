e2767d113c152e02f7c8ea08fa9b9e6b2bc1ab2b
Rodrigo <rodrigoml2395@gmail.com>
2023-02-18 17:02:57 -0300

    build: nest config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [RoutesModule],
+  imports: [RoutesModule, MongooseModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })

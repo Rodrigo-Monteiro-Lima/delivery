187f2672b2abeb30351079529bcf6376f8278bd8
Rodrigo <rodrigoml2395@gmail.com>
2023-02-18 17:21:30 -0300

    feat: Mongoose module connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
 
 @Module({
-  imports: [RoutesModule, MongooseModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    RoutesModule,
+    MongooseModule.forRoot(process.env.MONGO_DSN, {
+      useNewUrlParser: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/routes/routes.module.ts
+++ backend/src/routes/routes.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { RoutesService } from './routes.service';
 import { RoutesController } from './routes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RouteSchema, Route } from './entities/route.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Route.name, schema: RouteSchema }]),
+  ],
   controllers: [RoutesController],
-  providers: [RoutesService]
+  providers: [RoutesService],
 })
 export class RoutesModule {}
